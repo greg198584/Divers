993dbefa610de5e62db790d6ee474ea9a7e22e17
glafitte <greg.lafitte06@gmail.com>
2015-02-10 12:58:10 +0100

    changement de test

--- tdd/ft_calc.spec.c
+++ tdd/ft_calc.spec.c
@@ -6,43 +6,24 @@
 /*   By: glafitte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/10 11:28:31 by glafitte          #+#    #+#             */
-/*   Updated: 2015/02/10 12:39:53 by glafitte         ###   ########.fr       */
+/*   Updated: 2015/02/10 12:56:33 by glafitte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <moulitest.h>
+#include <string.h>
 
-int	ft_calc(char *calcul);
+int	ft_strcmp(char const *s1, char const *s2);
 
-static void	simple_add(t_test *test)
+static void	string_cmp(t_test *test)
 {
-	mt_assert(ft_calc("10 % 3") == 1);
+	char	*s1 = "coucou";
+	char	*s2 = "coucou";
+
+	mt_assert(ft_strcmp(s1, s2) == strcmp(s1, s2));
 }
 
-static void	larg_multi(t_test *test)
-{
-	mt_assert(ft_calc("100 * 100") == 10000);
-}
-
-static void	simple_modulo(t_test *test)
-{
-	mt_assert(ft_calc("10 % 3") == 1);
-}
-
-static void	simple_sub(t_test *test)
-{
-	mt_assert(ft_calc("10 - 5") == 5);
-}
-
-static void	glue_test(t_test *test)
-{
-	mt_assert(ft_calc("10+2") == 0);
-}
 void		suite_ft_calc(t_suite *suite)
 {
-	SUITE_ADD_TEST(suite, simple_add);
-	SUITE_ADD_TEST(suite, larg_multi);
-	SUITE_ADD_TEST(suite, simple_modulo);
-	SUITE_ADD_TEST(suite, simple_sub);
-	SUITE_ADD_TEST(suite, glue_test);
+	SUITE_ADD_TEST(suite, string_cmp);
 }
