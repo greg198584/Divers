f9cf392d3a9840c1b11956e9320fe4940fb4c3e7
Gregory LAFITTE <glafitte@e1r2p22.42.fr>
2015-02-14 16:57:52 +0100

    MaJ - ajout demo ncurses

--- tdd/ft_calc.spec.c
+++ tdd/ft_calc.spec.c
@@ -5,28 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: glafitte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/02/10 11:28:31 by glafitte          #+#    #+#             */
-/*   Updated: 2015/02/11 12:57:39 by glafitte         ###   ########.fr       */
+/*   Created: 2015/02/14 11:13:11 by glafitte          #+#    #+#             */
+/*   Updated: 2015/02/14 11:20:26 by glafitte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <moulitest.h>
-#include <string.h>
 
-int			ft_calc(char *calcul);
 
-static void	simple_calcul(t_test *test)
+int	ft_calc(char *calcul);
+
+static void	simple_add(t_test *test)
 {
-	mt_assert(ft_calc("4 + 4") == 8);
+	mt_assert(ft_calc("4+4 ") == 0);
 }
 
-static void	simple_calcul_2(t_test *test)
+static void	simple_multi(t_test *test)
 {
-	mt_assert(ft_calc("4+2") == 0);
+	mt_assert(ft_calc("4 * 2") == 8);
 }
 
-void		suite_ft_calc(t_suite *suite)
+static void	simple_add_false(t_test *test)
 {
-	SUITE_ADD_TEST(suite, simple_calcul);
-	SUITE_ADD_TEST(suite, simple_calcul_2);
+	mt_assert(ft_calc("4 + 2") == 6);
+}
+
+void	suite_ft_calc(t_suite *suite)
+{
+	SUITE_ADD_TEST(suite, simple_add);
+	SUITE_ADD_TEST(suite, simple_multi);
+	SUITE_ADD_TEST(suite, simple_add_false);
 }
